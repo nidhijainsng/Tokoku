64e5caac64594a730e0e07cc7b1c7df42d970766
Hardik27 <hardikruparel14@gmail.com>
2020-06-22 23:04:01 +0530

    Ajax call to the API on Dice Roll

--- js/templates/dice_template/rollingDice.js
+++ js/templates/dice_template/rollingDice.js
@@ -7,6 +7,10 @@ function pointRoll() {
   var diceValueOne   = Math.floor((Math.random() * 6) + 1);
   var audioElement = document.getElementById('rollIt');
   ubsApp.raiseAudioEvent(audioElement, 'rollingDice');
+  
+  hitScoreAPI();
+  
+  
   while(diceValueOne==rollingDiceConfig.currentRandomDiceOne){
     diceValueOne   = Math.floor((Math.random() * 6) + 1);
   }
@@ -22,3 +26,29 @@ function pointRoll() {
   }
   return diceValueOne;
 }
+
+function hitScoreAPI(){
+  console.log("Ajax call to the API");
+  var dataObject=getDataObject();
+  var data=JSON.parse(JSON.stringify(dataObject));  
+  console.log(data);
+  $.ajax({
+    type: 'post',
+    url: 'http://www.rpi.prathamskills.org/api/pushdatapradigi/post',
+    data: data,
+    dataType: 'jsonp',
+    //username: "_",
+    //password: "_",
+    traditional: true,
+    success: function (data) {
+        console.log("Successfully received the data from the server");
+        console.log(data);
+    }
+  });
+}
+//Generate Data Object based on the attributes to send to API
+function getDataObject(){
+  var tempObject={name:"John", age:"30", city:"New York"};
+  return tempObject;
+}
+
